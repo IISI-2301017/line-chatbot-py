836b23a4c93fee209b65dda4f29150184d42eaec
DamnTT <thomas.kao@cwntgroup.com>
2023-11-19 14:58:27 +0000

    connect line api

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ from linebot.models import *
 import os
 
 app = Flask(__name__)
-
-line_bot_api = LineBotApi(str(os.environ.get('CHANNEL_ACCESS_TOKENl')))
-handler = WebhookHandler(str(os.environ.get('CHANNEL_SECRET')))
+CHANNEL_ACCESS_TOKEN = os.environ.get("CHANNEL_ACCESS_TOKEN","IPfGFd0aLtai0SL6qgZOXHFAAKRdDMdsbtZIV8mCkeH7UiPfSydY0Dw2J/GSHLbCZasIFJy07yFOMpdYyIc5EJxrAvqEOu0mhSbTiEKAaRTsDjbUEWLz+yDfJt+eSqADDSkK/gAAguNTWcWIJwBLUAdB04t89/1O/w1cDnyilFU=")
+CHANNEL_SECRET = os.environ.get("CHANNEL_SECRET", "66f9192c153f1a643df44c69d7d39de3")
+print(CHANNEL_ACCESS_TOKEN)
+print(CHANNEL_SECRET)
+line_bot_api = LineBotApi(str(CHANNEL_ACCESS_TOKEN))
+handler = WebhookHandler(str(CHANNEL_SECRET))
 
 
 @app.route("/callback", methods=['POST'])
